c315c2b60d536879fa2d596204887b64ee5b9172
Rahul Rajput <rr611305@gmail.com>
2023-06-11 17:49:30 +0530

    login dialog setup

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,17 +1,10 @@
-import React from 'react'
+
 
 const User = () => {
     return (
+        <div>
+           
 
-        <div className="flex justify-center items-center h-screen">
-            <div className="flex flex-col justify-center items-center">
-                <h1 className="text-4xl font-bold">Login</h1>
-                <form className="flex flex-col justify-center items-center">
-                    <input className="border-2 border-gray-400 p-2 rounded-lg my-2" type="text" placeholder="Username" />
-                    <input className="border-2 border-gray-400 p-2 rounded-lg my-2" type="password" placeholder="Password" />
-                    <button className="bg-teal-500 text-white p-2 rounded-lg my-2">Login</button>
-                </form>
-            </div>
         </div>
 
     )
